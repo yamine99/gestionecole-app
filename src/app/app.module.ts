85448dccc8729384e60e71f9e234f2ec7a227bbf
yamine <salem.mohamedyamine@gmail.com>
2022-07-04 11:19:55 +0200

    [ADD] form of dialog-student

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,34 @@ import { StudentComponent } from './component/student/student.component';
 import { HeaderComponent } from './component/header/header.component';
 import { LoginComponent } from './component/login/login.component';
 import {HomeComponent} from "./component/home/home.component";
+import { DialogFormComponent } from './component/dialog-form/dialog-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatDialogModule} from "@angular/material/dialog";
 
+import {MatFormFieldModule} from "@angular/material/form-field";
+import { ReactiveFormsModule} from "@angular/forms";
+import {MatIconModule} from "@angular/material/icon";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {MatAutocompleteModule} from "@angular/material/autocomplete";
 @NgModule({
   declarations: [
     AppComponent,
     StudentComponent,
     HeaderComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    DialogFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatAutocompleteModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {DialogFormComponent} from "../dialog-form/dialog-form.component";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+
+  openDialogForm() {
+    this.dialog.open(DialogFormComponent);
+  }
 }
