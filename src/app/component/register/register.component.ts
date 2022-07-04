311b7d6ae465ad4a85080c674b1183f1d3a438b5
niama315 <aroubiteniama@gmail.com>
2022-07-04 17:13:46 +0200

    [ADD] page de connexion/ register

---
+++ src/app/component/register/register.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss']
+})
+export class RegisterComponent implements OnInit {
+  registerForm !: FormGroup;
+  constructor( private _formBuilder: FormBuilder, private router: Router) { }
+
+  ngOnInit(): void {
+
+    this.registerForm = this._formBuilder.group({
+      lastName:[null, Validators.required],
+      firstName:[null, Validators.required],
+      mail:[null, Validators.required],
+      password : [null, Validators.required],
+      confirmPassword : [null, Validators.required],
+    },{
+      updateOn:'blur'
+    });
+
+  }
+  onSubmit() {
+
+  }
+  back(){
+    this.router.navigateByUrl("/connexion").then(r => console.log(r));
+  }
+}
